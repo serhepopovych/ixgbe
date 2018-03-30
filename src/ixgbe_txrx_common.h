43bf5988419fe6b267f098ae2fb2cadd0a5e9ca1
Serhey Popovych <serhe.popovych@gmail.com>
2018-03-30 20:48:23 +0300

    ixgbe: Add support for Double VLAN mode
    There is no practical limit on number of stacked vlans in Linux which
    means multiple VLAN headers could be present in Ethernet frames.
    
    However it is less common to have stacking depth more than two which
    is known as 802.1ad or QinQ protocols.
    
    By default driver provides hardware offloading only for one VLAN header:
    it supports filtering by vlan id (vid), strip on receive, insert on
    transmit; as well as skipping header to provide offload for next
    protocol.
    
    In case of stacked vlans next protocol is also VLAN which is in turn
    isn't known to hardware without additional configuration. That means
    hardware does not provide any additional offload like receive hash
    calculation, direction to specific receive queue etc. Last one means
    that RSS does not work for packets with multiple VLAN headers.
    
    Hardware supports double vlans when configured in specific mode called
    Global Double VLAN in "Intel(R) 82599 10 GbE Controller Datasheet".
    
    With this change we implement support for this mode in ixgbe driver.
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- src/ixgbe_txrx_common.h
+++ src/ixgbe_txrx_common.h
@@ -63,7 +63,7 @@ bool ixgbe_xsk_any_rx_ring_enabled(struct ixgbe_adapter *adapter);
 bool ixgbe_cleanup_headers(struct ixgbe_ring __maybe_unused *rx_ring,
 			   union ixgbe_adv_rx_desc *rx_desc,
 			   struct sk_buff *skb);
-void ixgbe_process_skb_fields(struct ixgbe_ring *rx_ring,
+bool ixgbe_process_skb_fields(struct ixgbe_ring *rx_ring,
 			      union ixgbe_adv_rx_desc *rx_desc,
 			      struct sk_buff *skb);
 void ixgbe_rx_skb(struct ixgbe_q_vector *q_vector,
